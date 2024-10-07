f040e01213031c35ba5643d489d33a4a479dc640
Ruhan <ruhan_350@hotmail.com>
2024-10-07 00:13:11 -0300

    Implementado a tela inicial do APP conforme exemplo

--- App.tsx
+++ App.tsx
@@ -1,11 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Button, StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.productImages}>
+        <Image
+          source={{ uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR92RibS6nXGww1R7R2dYrHb53A0aS9CbjE5w&s' }}
+          style={styles.image}
+        />
+        <Image
+          source={{ uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR92RibS6nXGww1R7R2dYrHb53A0aS9CbjE5w&s' }}
+          style={styles.image}
+        />
+        <Image
+          source={{ uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR92RibS6nXGww1R7R2dYrHb53A0aS9CbjE5w&s' }}
+          style={styles.image}
+        />
+      </View>
+
+      <Text style={styles.title}>Avalie Aqui</Text>
+      <Text style={styles.subtitle}>
+        Escolha o produto que você deseja avaliar e compartilhe sua experiência com outros consumidores.
+        </Text>
+
+        <Button title="Entrar" onPress={() => {}} />
     </View>
   );
 }
@@ -16,5 +35,27 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 16,
   },
+  productImages: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 20,
+  },
+  image: {
+    width: 100,
+    height: 150,
+    resizeMode: 'contain',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  subtitle: {
+    fontSize: 16,
+    color: 'gray',
+    textAlign: 'center',
+    marginBottom: 20,
+  }
 });
