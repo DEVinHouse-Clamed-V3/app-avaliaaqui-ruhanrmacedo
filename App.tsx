392e738f91318d6db9a4a4ffc2d32bd1472376f3
Ruhan <ruhan_350@hotmail.com>
2024-10-07 17:17:46 -0300

    Implementado tela de listagem de produtos com requisição GET

--- App.tsx
+++ App.tsx
@@ -1,30 +1,53 @@
-import React from 'react';
-import { Button, StyleSheet, Text, View, Image } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Button, StyleSheet, Text, View, Image, TouchableOpacity, FlatList } from 'react-native';
+import axios from 'axios';
 
-export default function App() {
+export default function App({ navigation }) {
+  const [products, setProducts] = useState([]);
+
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('http://192.168.1.103:3000/products');
+      setProducts(response.data); 
+    } catch (error) {
+      console.error('Erro ao buscar produtos: ', error);
+    }
+  };
+
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+
+  const renderProducts = ({ item }) => { 
+    return (
+      <View style={styles.productCard}>
+        <Image source={{ uri: item.image }} style={styles.productImage} />
+        <View style={styles.productInfo}>
+          <Text style={styles.productName}>{item.name}</Text>
+          <Text style={styles.productBrand}>Marca: {item.brand}</Text>
+          <Text style={styles.productDescription}>{item.description}</Text>
+          <Text style={styles.productPrice}>{item.price}</Text>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => navigation.navigate('Avaliacao', { productId: item.id })}
+          >
+            <Text style={styles.buttonText}>Avaliar</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  };
+
   return (
     <View style={styles.container}>
-      <View style={styles.productImages}>
-        <Image
-          source={{ uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR92RibS6nXGww1R7R2dYrHb53A0aS9CbjE5w&s' }}
-          style={styles.image}
-        />
-        <Image
-          source={{ uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR92RibS6nXGww1R7R2dYrHb53A0aS9CbjE5w&s' }}
-          style={styles.image}
-        />
-        <Image
-          source={{ uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR92RibS6nXGww1R7R2dYrHb53A0aS9CbjE5w&s' }}
-          style={styles.image}
-        />
-      </View>
-
-      <Text style={styles.title}>Avalie Aqui</Text>
-      <Text style={styles.subtitle}>
-        Escolha o produto que você deseja avaliar e compartilhe sua experiência com outros consumidores.
-        </Text>
-
-        <Button title="Entrar" onPress={() => {}} />
+      <FlatList
+        data={products} 
+        keyExtractor={(item) => item.id.toString()} 
+        renderItem={renderProducts} 
+      />
     </View>
   );
 }
@@ -33,29 +56,52 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
     padding: 16,
   },
-  productImages: {
+  productCard: {
     flexDirection: 'row',
-    justifyContent: 'space-between',
-    marginBottom: 20,
+    marginBottom: 16,
+    backgroundColor: '#f9f9f9',
+    padding: 10,
+    borderRadius: 8,
   },
-  image: {
+  productImage: {
     width: 100,
     height: 150,
     resizeMode: 'contain',
+    marginRight: 16,
   },
-  title: {
-    fontSize: 24,
+  productInfo: {
+    flex: 1,
+  },
+  productName: {
+    fontSize: 18,
     fontWeight: 'bold',
-    marginBottom: 10,
+    marginBottom: 4,
   },
-  subtitle: {
-    fontSize: 16,
+  productBrand: {
+    fontSize: 14,
     color: 'gray',
+    marginBottom: 4,
+  },
+  productDescription: {
+    fontSize: 14,
+    marginBottom: 8,
+  },
+  productPrice: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginBottom: 8,
+  },
+  button: {
+    backgroundColor: '#e60000',
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    borderRadius: 8,
+  },
+  buttonText: {
+    color: '#fff',
     textAlign: 'center',
-    marginBottom: 20,
-  }
+    fontWeight: 'bold',
+  },
 });
